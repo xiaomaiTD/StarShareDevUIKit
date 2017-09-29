4dfb284363bd0552f1293118b602621f6424b34b
wangrui <wr@sunako.org>
2017-09-29 13:56:29 +0800

    Add public header

---
+++ StarShareDevUIKit/StarShareDevUIKit/StarShareDevUIKit.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  StarShareDevUIKit
+//
+//  Created by jearoc on 2017/9/29.
+//  Copyright © 2017年 jearoc. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
