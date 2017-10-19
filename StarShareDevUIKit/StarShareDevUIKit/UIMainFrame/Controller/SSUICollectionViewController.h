61b21d5b77b8188a3168de48fc2e34b050e9db63
wangrui <wr@sunako.org>
2017-10-19 17:15:07 +0800

    no message

--- StarShareDevUIKit/StarShareDevUIKit/UIMainFrame/Controller/SSUICollectionViewController.h
+++ StarShareDevUIKit/StarShareDevUIKit/UIMainFrame/Controller/SSUICollectionViewController.h
@@ -11,6 +11,10 @@
 
 @interface SSUICollectionViewController : SSUIViewController<SSUICollectionViewDelegate,SSUICollectionViewDataSource>
 @property(nonatomic, strong, readonly) SSUICollectionView *collectionView;
+
+/**
+ 修改 UICollectionView ContentInset ，一旦修改了此属性并且不等于 {-1, -1, -1, -1} 的时候，系统的 automaticallyAdjustsScrollViewInsets 和 iOS 11 下的新属性 contentInsetAdjustmentBehavior 可能会影响到 ContentInset | adjustedContentInset 其实这个时候 UICollectionView ContentInset 并不是等于你设置的 collectionViewInitialContentInset ，所以在这个时候如果想要使用此属性 必须设置 automaticallyAdjustsScrollViewInsets = NO，或者 contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever。
+ */
 @property(nonatomic, assign) UIEdgeInsets collectionViewInitialContentInset;
 @property(nonatomic, assign) UIEdgeInsets collectionViewInitialScrollIndicatorInsets;
 @end
--- StarShareDevUIKit/StarShareDevUIKit/UIMainFrame/Controller/SSUIListViewController.h
+++ StarShareDevUIKit/StarShareDevUIKit/UIMainFrame/Controller/SSUIListViewController.h
@@ -17,6 +17,10 @@
 - (void)didInitializedWithStyle:(UITableViewStyle)style NS_REQUIRES_SUPER;
 @property(nonatomic, assign, readonly) UITableViewStyle style;
 @property(nonatomic, strong, readonly) SSUITableView *tableView;
+
+/**
+ 修改 UITableView ContentInset ，一旦修改了此属性并且不等于 {-1, -1, -1, -1} 的时候，系统的 automaticallyAdjustsScrollViewInsets 和 iOS 11 下的新属性 contentInsetAdjustmentBehavior 可能会影响到 ContentInset | adjustedContentInset 其实这个时候 UITableView ContentInset 并不是等于你设置的 tableViewInitialContentInset ，所以在这个时候如果想要使用此属性 必须设置 automaticallyAdjustsScrollViewInsets = NO，或者 contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever。
+ */
 @property(nonatomic, assign) UIEdgeInsets tableViewInitialContentInset;
 @property(nonatomic, assign) UIEdgeInsets tableViewInitialScrollIndicatorInsets;
 - (void)hideTableHeaderViewInitialIfCanWithAnimated:(BOOL)animated force:(BOOL)force;
